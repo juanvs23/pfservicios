78b0a6fc762261b7a9b74627361fd145a61d3359
Juan Carlos Avila <juanvs23@gmail.com>
2024-05-04 13:29:24 -0400

    bajando cambios

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
     telephone: 584141220527,
     siteProdUrl:'https://pfservicios.vercel.app',
     siteDevUrl:'http://localhost:5173',
-    devServerUrl:'http://localhost:8000',
+    devServerUrl:'https://pfservicios-back.vercel.app',
     prodServerUrl:'https://pfservicios.vercel.app',
     secretCode: 'pfservicios123',
 }
